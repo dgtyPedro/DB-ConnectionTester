61cb3561ca9f0b01b559f3f8b4ae25c557aaea36
dgtyPedro <pedrogcs0104@gmail.com>
2022-05-31 17:03:53 -0300

    pending commits

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,10 +1,6 @@
 <template>
-<a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
-    <i class="fa fa-whatsapp my-float"></i>
-</a>
-<div class="modal">
-	aaa
-</div>
+<a href="https://github.com/dgtyPedro/DB-ConnectionTester/blob/main/README.md" class="float" target="_blank">
+	<i class="fa-solid fa-question icon-float"></i></a> 
 </template>
 
 <script>
@@ -21,7 +17,7 @@
 	height:60px;
 	bottom:40px;
 	right:40px;
-	background-color:#25d366;
+	background-color:var(--color-pinky);
 	color:#FFF;
 	border-radius:50px;
 	text-align:center;
@@ -30,20 +26,8 @@
 	z-index:100;
 }
 
-.my-float{
+.icon-float{
 	margin-top:16px;
 }
-
-.modal{
-        color: var(--color-orangy);
-        font-size: 1.5rem;
-        background-color: rgba(255,255,255, 0.9);
-        height: fit-content;
-        width: 500px;   
-        border-radius: 10px;
-        -webkit-box-shadow: 5px 5px 14px 5px var(--color-orangy); 
-        box-shadow: 5px 5px 14px 5px var(--color-orangy);
-}
-
 
 </style>
